20d12e2a4c2dbaac904952ca1396ffcf6e3cb37b
Ahmed Rezk <ahmed.rezk@principlestudios.com>
2022-01-02 22:23:05 -0700

    Update hammerspoon init

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,11 +3,6 @@ hs.hints.showTitleThresh = 0
 hs.window.animationDuration = 0
 hs.logger.defaultLogLevel = "info"
 
-local window = require "hs.window"
-
-hyper = { "cmd", "alt", "shift", "ctrl" }
--- meh = { "alt", "shift", "ctrl" }
-
 hs.loadSpoon "SpoonInstall"
 
 Install = spoon.SpoonInstall
@@ -15,114 +10,79 @@ Install = spoon.SpoonInstall
 Install:andUse("Reload", {
 	hotkeys = { reload = { { "cmd", "shift", "ctrl" }, "R" } },
 })
---[[ Install:andUse("Tunnelblick", {
-	config = {
-		username = "tyler.thrailkill",
-		password_fn = function()
-			return spoon.Keychain:login_keychain "token_tunnel_pass" .. spoon.Token:get_token()
-		end,
-		connection_name = "promontech-openvpn",
+
+-- ensure IPC is there
+hs.ipc.cliInstall()
+
+-- lower logging level for hotkeys
+require("hs.hotkey").setLogLevel "warning"
+
+-- global config
+config = {
+	apps = {
+		terms = { "kitty" },
+		browsers = { "Google Chrome", "Safari" },
 	},
-}) ]]
 
--- hs.loadSpoon "Ki"
--- spoon.Ki.workflowEvents = { ... } -- configure `spoon.Ki` here
+	wm = {
+		defaultDisplayLayouts = {
+			["Color LCD"] = "monocle",
+			["DELL U3818DW"] = "main-center",
+		},
 
---[[ wm = require "wm"
-layout = require "wm.layout"
-local utils = require "wm.utils" ]]
+		displayLayouts = {
+			["Color LCD"] = { "monocle", "main-right", "side-by-side" },
+			["DELL U3818DW"] = { "main-center", "main-right", "side-by-side" },
+		},
+	},
 
---[[ local direction = utils.direction
-local orientation = utils.orientation ]]
+	window = {
+		highlightBorder = false,
+		highlightMouse = true,
+		historyLimit = 0,
+	},
 
---[[ function l()
-	return wm.controller.screenLayout
-end
-function w()
-	return wm.controller.screenLayout:selectedWorkspace()
-end ]]
+	network = {
+		home = "Skynet 5G",
+	},
 
-hs.window.animationDuration = 0
-
---[[ units = {
-	left = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
-	right = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
-	up = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
-	down = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
-
-	topleft = { x = 0.00, y = 0.00, w = 0.50, h = 0.50 },
-	topright = { x = 0.50, y = 0.00, w = 0.50, h = 0.50 },
-	bottomleft = { x = 0.00, y = 0.50, w = 0.50, h = 0.50 },
-	bottomright = { x = 0.50, y = 0.50, w = 0.50, h = 0.50 },
-
-	left13 = { x = 0.00, y = 0.00, w = 0.33, h = 1.00 },
-	left23 = { x = 0.00, y = 0.00, w = 0.67, h = 1.00 },
-	center13 = { x = 0.33, y = 0.00, w = 0.33, h = 1.00 },
-	right23 = { x = 0.33, y = 0.00, w = 0.67, h = 1.00 },
-	right13 = { x = 0.67, y = 0.00, w = 0.33, h = 1.00 },
-
-	center = { x = 0.20, y = 0.10, w = 0.60, h = 0.80 },
-	maximum = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
+	homebridge = {
+		studioSpeakers = { aid = 10, iid = 11, name = "Studio Speakers" },
+		studioLights = { aid = 9, iid = 11, name = "Studio Lights" },
+		tvLights = { aid = 6, iid = 11, name = "TV Lights" },
+	},
 }
 
-mash = { "ctrl", "alt" }
-hs.hotkey.bind(mash, "left", function()
-	hs.window.focusedWindow():move(units.left, nil, true)
-end)
-hs.hotkey.bind(mash, "right", function()
-	hs.window.focusedWindow():move(units.right, nil, true)
-end)
-hs.hotkey.bind(mash, "up", function()
-	hs.window.focusedWindow():move(units.up, nil, true)
-end)
-hs.hotkey.bind(mash, "down", function()
-	hs.window.focusedWindow():move(units.down, nil, true)
-end)
-
-hs.hotkey.bind(mash, "u", function()
-	hs.window.focusedWindow():move(units.topleft, nil, true)
-end)
-hs.hotkey.bind(mash, "i", function()
-	hs.window.focusedWindow():move(units.topright, nil, true)
-end)
-hs.hotkey.bind(mash, "j", function()
-	hs.window.focusedWindow():move(units.bottomleft, nil, true)
-end)
-hs.hotkey.bind(mash, "k", function()
-	hs.window.focusedWindow():move(units.bottomright, nil, true)
-end)
-
-hs.hotkey.bind(mash, "d", function()
-	hs.window.focusedWindow():move(units.left13, nil, true)
-end)
-hs.hotkey.bind(mash, "e", function()
-	hs.window.focusedWindow():move(units.left23, nil, true)
-end)
-hs.hotkey.bind(mash, "f", function()
-	hs.window.focusedWindow():move(units.center13, nil, true)
-end)
-hs.hotkey.bind(mash, "g", function()
-	hs.window.focusedWindow():move(units.right23, nil, true)
-end)
-hs.hotkey.bind(mash, "t", function()
-	hs.window.focusedWindow():move(units.right13, nil, true)
-end)
-
-hs.hotkey.bind(mash, "c", function()
-	hs.window.focusedWindow():move(units.center, nil, true)
-end)
-hs.hotkey.bind(mash, "return", function()
-	hs.window.focusedWindow():move(units.maximum, nil, true)
-end) ]]
-
+-- requires
 bindings = require "bindings"
+controlplane = require "utils.controlplane"
+-- watchables = require "utils.watchables"
+watchers = require "utils.watchers"
 wm = require "utils.wm"
 
+-- no animations
+hs.window.animationDuration = 0.0
+
+-- hints
+hs.hints.fontName = "Helvetica-Bold"
+hs.hints.fontSize = 22
+hs.hints.hintChars = { "A", "S", "D", "F", "J", "K", "L", "Q", "W", "E", "R", "Z", "X", "C" }
+hs.hints.iconAlpha = 1.0
+hs.hints.showTitleThresh = 0
+
+-- controlplane
+controlplane.enabled = { "autohome", "automount" }
+
+-- watchers
+watchers.enabled = { "urlevent" }
+watchers.urlPreference = config.apps.browsers
+
 -- bindings
-bindings.enabled = { "focus", "global", "tiling" }
+bindings.enabled = { "ask-before-quit", "block-hide", "ctrl-esc", "f-keys", "focus", "global", "tiling", "term-ctrl-i", "viscosity" }
+bindings.askBeforeQuitApps = config.apps.browsers
 
 -- start/stop modules
-local modules = { bindings, wm }
+local modules = { bindings, controlplane, watchers, wm }
 
 hs.fnutils.each(modules, function(module)
 	if module then
@@ -138,120 +98,6 @@ hs.shutdownCallback = function()
 		end
 	end)
 end
-
---[[ local hhtwm = require "hhtwm"
-hhtwm.start() ]]
-
---[[ hs.hotkey.bind({ "alt" }, "h", function()
-	w():focus(direction.left)
-end)
-hs.hotkey.bind({ "alt" }, "j", function()
-	w():focus(direction.down)
-end) ]]
-
---[[
-hs.hotkey.bind({ "alt" }, "k", function()
-	w():focus(direction.up)
-end)
-hs.hotkey.bind({ "alt" }, "l", function()
-	w():focus(direction.right)
-end)
-
-hs.hotkey.bind({ "alt" }, "a", function()
-	w():selectParent()
-end)
-hs.hotkey.bind({ "alt" }, "d", function()
-	w():selectChild()
-end)
-hs.hotkey.bind({ "alt", "shift" }, "f", function()
-	w():showFocus()
-end)
-hs.hotkey.bind({ "alt", "shift" }, "q", function()
-	w():closeSelected()
-end)
-
-hs.hotkey.bind({ "alt", "shift" }, "h", function()
-	w():move(direction.left)
-end)
-hs.hotkey.bind({ "alt", "shift" }, "j", function()
-	w():move(direction.down)
-end)
-hs.hotkey.bind({ "alt", "shift" }, "k", function()
-	w():move(direction.up)
-end)
-hs.hotkey.bind({ "alt", "shift" }, "l", function()
-	w():move(direction.right)
-end)
-
-hs.hotkey.bind({ "alt" }, "y", function()
-	w():resize(direction.left, 0.1)
-end)
-hs.hotkey.bind({ "alt" }, "u", function()
-	w():resize(direction.down, 0.1)
-end)
-hs.hotkey.bind({ "alt" }, "i", function()
-	w():resize(direction.up, 0.1)
-end)
-hs.hotkey.bind({ "alt" }, "o", function()
-	w():resize(direction.right, 0.1)
-end)
-hs.hotkey.bind({ "alt", "shift" }, "y", function()
-	w():resize(direction.left, -0.1)
-end)
-hs.hotkey.bind({ "alt", "shift" }, "u", function()
-	w():resize(direction.down, -0.1)
-end)
-hs.hotkey.bind({ "alt", "shift" }, "i", function()
-	w():resize(direction.up, -0.1)
-end)
-hs.hotkey.bind({ "alt", "shift" }, "o", function()
-	w():resize(direction.right, -0.1)
-end)
-
-hs.hotkey.bind({ "alt" }, "s", function()
-	w():setMode(layout.mode.stacked)
-end)
-hs.hotkey.bind({ "alt" }, "t", function()
-	w():setMode(layout.mode.tabbed)
-end)
-hs.hotkey.bind({ "alt" }, "e", function()
-	w():setMode(layout.mode.default)
-end)
-
-hs.hotkey.bind({ "alt" }, "x", function()
-	l():addWindow(hs.window.focusedWindow())
-end)
-hs.hotkey.bind({ "alt", "cmd" }, "x", function()
-	l():removeWindow(hs.window.focusedWindow())
-end)
-hs.hotkey.bind({ "alt", "shift" }, "space", function()
-	w():toggleFloating()
-end)
-hs.hotkey.bind({ "alt" }, "space", function()
-	w():toggleFocusMode()
-end)
-
-hs.hotkey.bind({ "alt" }, "f", function()
-	w():toggleFullscreen()
-end)
-
-hs.hotkey.bind({ "alt" }, "z", function()
-	wm.tracker:stop()
-end)
-hs.hotkey.bind({ "alt", "shift" }, "z", function()
-	wm.tracker:start()
-end)
-
-hs.hotkey.bind({ "alt" }, "-", function()
-	w():splitCurrent(orientation.vertical)
-end)
-hs.hotkey.bind({ "alt" }, "\\", function()
-	w():splitCurrent(orientation.horizontal)
-end)
-
-hs.hotkey.bind({ "shift", "alt" }, "s", function()
-	hs.spotify.displayCurrentTrack()
-end) ]]
 
 --[[ hs.notify.new({
 	title = "Succuss",
