b336f7811838ccef753d7f8b543d346522874957
Ahmed Rezk <rezk5024@gmail.com>
2025-07-25 18:21:11 -0600

    🔧 (hammerspoon/init.lua): update init script to include new extra module 🔧 (hammerspoon/extra/init.lua): add new init file for extra modules 🔧 (hammerspoon/extra/loadKeyboard.lua): add new loadKeyboard module to open webview with URL 🔧 (.luarc.json): add services and service to globals in luarc configuration

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,16 +1,14 @@
 spoon = spoon
 hs = hs
 
+Settings = require("settings")
 log = require("logger")
-local Settings = require("settings")
+require("extra")
 require("ai")
 
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 hs.alert.show("Config loaded 🔨🥄")
-
--- Thie plugin generates EmmyLua annotations for Hammerspoon and any installed Spoons.
-hs.loadSpoon("EmmyLua")
 
 hs.loadSpoon("MicMute")
 
@@ -66,6 +64,7 @@ end)
 
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "r", AskOpenAIStreaming)
 hs.hotkey.bind(Settings.keys.HYPER, "c", GrammerCheck)
+
 -- test w/ T
 hs.hotkey.bind(Settings.keys.MEH, "b", function()
 	hs.alert.show("Config loaded 🔨🥄")
@@ -77,72 +76,5 @@ require("config.uielements")
 require("config.ui_callouts")
 require("config.observer")
 
--- local end_time = hs.timer.secondsSinceEpoch()
--- print("init.lua took", end_time - start_time, "seconds")
---
--- caffeine = hs.menubar.new()
--- function setCaffeineDisplay(state)
--- 	if state then
--- 		caffeine:setTitle("🚀 AWAKE")
--- 	else
--- 		caffeine:setTitle("🚀 SLEEPY")
--- 	end
--- end
---
--- function caffeineClicked()
--- 	setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
--- end
---
--- if caffeine then
--- 	caffeine:setClickCallback(caffeineClicked)
--- 	setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
--- end
---
---
-
--- Function to load HTML content from a URL
-function loadHTMLFromURL(url)
-	-- Get the current screen's frame
-	local screenFrame = hs.screen.primaryScreen():frame()
-	-- print("screenFrame " .. hs.screen.primaryScreen():frame())
-
-	-- Define desired webview dimensions
-	local webviewWidth = 1800
-	local webviewHeight = 800
-
-	-- Calculate the position to center the webview
-	local webviewX = screenFrame.x + (screenFrame.w - webviewWidth) / 2
-	local webviewY = screenFrame.y + (screenFrame.h - webviewHeight) / 2
-
-	-- Create a new webview
-	local webviewRect = hs.geometry.rect(webviewX, webviewY, webviewWidth, webviewHeight)
-
-	local webview = hs.webview.new(webviewRect)
-	webview:closeOnEscape()
-	webview:url(url)
-	webview
-		:bringToFront(true)
-		:windowStyle({ "HUD", "utility", "titled", "closable" })
-		:shadow(true)
-		:closeOnEscape(true)
-		:allowGestures(true)
-		:allowNewWindows(false)
-		:windowTitle("ddddddd")
-		:deleteOnClose(true)
-
-	webview:show()
-
-	--NOTE: WOW What a good meanbar
-	-- local closeButton = hs.menubar.new()
-	-- closeButton:setTitle("X")
-	-- closeButton:setClickCallback(function()
-	-- 	webview:delete() -- Close the webview
-	-- 	closeButton:delete() -- Remove the menubar item
-	-- end)
-end
-
--- Bind a key to trigger the function
-hs.hotkey.bind(Settings.keys.ULTRA, "m", function()
-	local url = "https://github.com/ahmed-rezk-dev/zmk-keyboard/blob/main/config/cradio.keymap"
-	loadHTMLFromURL(url)
-end)
+-- Thie plugin generates EmmyLua annotations for Hammerspoon and any installed Spoons.
+hs.loadSpoon("EmmyLua")
