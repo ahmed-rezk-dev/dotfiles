9ab567930901fbeb6337145581f4f5c3116e6cfd
Ahmed Rezk <rezk5024@gmail.com>
2025-07-25 20:56:05 -0600

    ✨ (hammerspoon/ai/chat.lua, init.lua, tasks.lua): add AI chat and task modules for enhanced automation capabilities

---
+++ hammerspoon/ai/init.lua
@@ -0,0 +1,21 @@
+services = require("config.ask.services")
+service = services.getService()
+require("ai.chat")
+require("ai.tasks")
+
+local prompt = require("playground/prompter").prompt
+local webChooser = require("playground.webchooser").run
+hs.hotkey.bind(Settings.keys.ULTRA, "k", function()
+  log.d("inininin")
+  -- local frame = hs.screen.mainScreen():fullFrame()
+  -- frame {
+  --   x = frame.x + frame.w * 0.15 / 2,
+  --   y = frame.y + frame.h * 0.25 / 2,
+  --   w = frame.w * 0.85,
+  --   h = frame.h * 0.75,
+  --
+  -- }
+
+  -- webChooser()
+  -- prompt("sfsfsfsdsfsf")
+end)
