9ed50f45c58cc37cd305e6a47f7acc747a41e853
Ahmed Rezk <work72019@gmail.com>
2026-02-06 14:19:20 -0700

    feat: add opencode agent plugins and update nvim configurations
    - Add opencode-agent-memory and oh-my-opencode plugins
    - Configure MCP servers with memory and Figma support
    - Fix gitsigns staged signs highlighting
    - Update multicursor plugin configuration
    - Update lazy-lock.json with latest plugin versions

--- nvim/lua/plugins/multicursor.lua
+++ nvim/lua/plugins/multicursor.lua
@@ -1,68 +1,144 @@
 return {
   "jake-stewart/multicursor.nvim",
   branch = "1.0",
+  keys = {
+    {
+      "<Up>",
+      function()
+        require("multicursor-nvim").lineAddCursor(-1)
+      end,
+      mode = { "n", "x" },
+      desc = "Add cursor above",
+    },
+    {
+      "<down>",
+      function()
+        require("multicursor-nvim").lineAddCursor(1)
+      end,
+      mode = { "n", "x" },
+      desc = "Add cursor below",
+    },
+    {
+      "<leader><S-up>",
+      function()
+        require("multicursor-nvim").lineSkipCursor(-1)
+      end,
+      mode = { "n", "x" },
+      desc = "Skip cursor above",
+    },
+    {
+      "<leader><S-down>",
+      function()
+        require("multicursor-nvim").lineSkipCursor(1)
+      end,
+      mode = { "n", "x" },
+      desc = "Skip cursor below",
+    },
+    {
+      "<leader>n",
+      function()
+        require("multicursor-nvim").matchAddCursor(1)
+      end,
+      mode = { "n", "x" },
+      desc = "Match add cursor forward",
+    },
+    {
+      "<leader>s",
+      function()
+        require("multicursor-nvim").matchSkipCursor(1)
+      end,
+      mode = { "n", "x" },
+      desc = "Match skip cursor forward",
+    },
+    {
+      "<leader>N",
+      function()
+        require("multicursor-nvim").matchAddCursor(-1)
+      end,
+      mode = { "n", "x" },
+      desc = "Match add cursor backward",
+    },
+    {
+      "<leader>S",
+      function()
+        require("multicursor-nvim").matchSkipCursor(-1)
+      end,
+      mode = { "n", "x" },
+      desc = "Match skip cursor backward",
+    },
+    {
+      "<c-leftmouse>",
+      function()
+        require("multicursor-nvim").handleMouse()
+      end,
+      mode = "n",
+      desc = "Mouse add cursor",
+    },
+    {
+      "<c-leftdrag>",
+      function()
+        require("multicursor-nvim").handleMouseDrag()
+      end,
+      mode = "n",
+      desc = "Mouse drag cursor",
+    },
+    {
+      "<c-leftrelease>",
+      function()
+        require("multicursor-nvim").handleMouseRelease()
+      end,
+      mode = "n",
+      desc = "Mouse release cursor",
+    },
+    {
+      "<c-q>",
+      function()
+        require("multicursor-nvim").toggleCursor()
+      end,
+      mode = { "n", "x" },
+      desc = "Toggle cursor",
+    },
+  },
   config = function()
     local mc = require("multicursor-nvim")
-
     mc.setup()
 
-    -- Add key mappings
-    local set = vim.keymap.set
+    -- Mappings defined in a keymap layer only apply when there are
+    -- multiple cursors. This lets you have overlapping mappings.
+    mc.addKeymapLayer(function(layerSet)
+      -- Add cursors above/below (only when multicursor active)
+      layerSet({ "n", "x" }, "<Up>", function()
+        mc.lineAddCursor(-1)
+      end)
+      layerSet({ "n", "x" }, "<Down>", function()
+        mc.lineAddCursor(1)
+      end)
 
-    -- Add cursors above/below the main cursor
-    set({"n", "x"}, "<c-k>", function() mc.addCursor("k") end)
-    set({"n", "x"}, "<c-j>", function() mc.addCursor("j") end)
+      -- Select a different cursor as the main one.
+      layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+      layerSet({ "n", "x" }, "<right>", mc.nextCursor)
 
-    -- Add a cursor and jump to the next word under cursor
-    set({"n", "x"}, "<c-n>", function() mc.addCursor("*") end)
+      -- Delete the main cursor.
+      layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
 
-    -- Jump to the next word under cursor but do not add a cursor
-    set({"n", "x"}, "<c-s>", function() mc.skipCursor("*") end)
+      -- Enable and clear cursors using escape.
+      layerSet("n", "<esc>", function()
+        if not mc.cursorsEnabled() then
+          mc.enableCursors()
+        else
+          mc.clearCursors()
+        end
+      end)
+    end)
 
-    -- Rotate the main cursor
-    set({"n", "x"}, "<left>", mc.nextCursor)
-    set({"n", "x"}, "<right>", mc.prevCursor)
-
-    -- Delete the main cursor
-    set({"n", "x"}, "<leader>x", mc.deleteCursor)
-
-    -- Add and remove cursors with mouse
-    set("n", "<c-leftmouse>", mc.handleMouse)
-
-    -- Easy way to add and remove cursors using the main cursor
-    set({"n", "x"}, "<c-q>", mc.toggleCursor)
-
-    -- Clone every cursor and disable the originals
-    set({"n", "x"}, "<leader><c-q>", mc.duplicateCursors)
-
-    -- Align cursor columns
-    set("n", "<leader>a", mc.alignCursors)
-
-    -- Split visual selections by regex
-    set("x", "S", mc.splitCursors)
-
-    -- Append/insert for each line of visual selections
-    set("x", "I", mc.insertVisual)
-    set("x", "A", mc.appendVisual)
-
-    -- Match new cursors within visual selections by regex
-    set("x", "M", mc.matchCursors)
-
-    -- Rotate visual selection contents
-    set("x", "<leader>t", function() mc.transposeCursors(1) end)
-    set("x", "<leader>T", function() mc.transposeCursors(-1) end)
-
-    -- Jumplist support
-    set({"n", "x"}, "<c-o>", mc.jumpForward)
-    set({"n", "x"}, "<c-i>", mc.jumpBackward)
-
-    -- Customize how cursors look
+    -- Customize how cursors look.
     local hl = vim.api.nvim_set_hl
-    hl(0, "MultiCursorCursor", { link = "Cursor" })
+    hl(0, "MultiCursorCursor", { reverse = true })
     hl(0, "MultiCursorVisual", { link = "Visual" })
-    hl(0, "MultiCursorSign", { link = "SignColumn"})
-    hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
+    hl(0, "MultiCursorSign", { link = "SignColumn" })
+    hl(0, "MultiCursorMatchPreview", { link = "Search" })
+    hl(0, "MultiCursorDisabledCursor", { reverse = true })
     hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-    hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
+    hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
   end,
 }
